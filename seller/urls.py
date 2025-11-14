9195548a8eb3731c30c62bca80cfdd18d6e733f8
Athul666MS <athulsivanandan294@gmail.com>
2025-11-14 17:25:57 +0530

    urls and views started

---
+++ seller/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from user import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("view/",views.view_product)
+]
