897cbc5cf3f526ef62b8e3b64e5643d59cb0c4a3
Athul666MS <athulsivanandan294@gmail.com>
2025-11-16 11:13:45 +0530

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from user import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   
     path("products/",views.products),
     path("register/",views.user_register),
     path("login/",views.user_login)
