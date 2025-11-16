35ef3482b4d0d865727e2d0645bb0ba7fe756f93
Athul666MS <athulsivanandan294@gmail.com>
2025-11-16 08:21:03 +0530

    half completed add product

--- seller/urls.py
+++ seller/urls.py
@@ -4,5 +4,6 @@ from seller import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("view/",views.view_product)
+    path("view/",views.view_product),
+    path('add/',views.add_product)
 ]
--- seller/views.py
+++ seller/views.py
@@ -1,11 +1,43 @@
+from django.contrib.auth import authenticate
+from django.utils.text import slugify
+from django.shortcuts import render, redirect
 
-from django.shortcuts import render
-from  .models import Product
+from core.models import SubCategory
+from .models import Product, SellerDetails
+
 
 def view_product(request):
     products=Product.objects.all()
     return render(request,"seller/sellerdashboard.html",{"product":products})
 
-def update_product(request):
+def add_product(request):
+    if request.method=="POST":
+        print(".,,,")
+        seller = SellerDetails.objects.get(user=request.user)
+        subcategory_id = request.POST.get("subcategory")
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        price = request.POST.get("price")
+        stock = request.POST.get("stock")
+        color = request.POST.get("color")
+        size = request.POST.get("size")
+        slug = slugify(name)
+        subcategory = SubCategory.objects.get(id=subcategory_id)
+
+        Product.objects.create(
+            seller=seller,
+            subcategory=subcategory,
+            name=name,
+            slug=slug,
+            description=description,
+            price=price,
+            stock=stock,
+            color=color,
+            size=size,
+        )
+        return redirect("seller_dashboard")
+
     products = Product.objects.all()
-    return  render(request,"seller/features.html")
+    subcategories = SubCategory.objects.all()
+    return render(request, "seller/features.html", {"subcategories": subcategories,"products":products})
+
