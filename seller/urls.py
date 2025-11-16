5d0e5fa247f2a8ce5d6fe30502b116fdca278910
Athul666MS <athulsivanandan294@gmail.com>
2025-11-16 13:56:54 +0530

    product update completed

--- seller/urls.py
+++ seller/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('registration/',views.seller_registration),
     path("seller_dashboard/",views.view_product,name="seller_dashboard"),
     path("login/",views.login_seller),
-    path("seller_dashboard/add/",views.add_product,name='add')
+    path("seller_dashboard/add/",views.add_product,name='add'),
+    path("seller_dashboard/update/<int:id>",views.update_product,name='update')
 ]
