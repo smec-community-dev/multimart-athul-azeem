1e117e6524b07832857cc5948b511550741bacc8
azeem7902 <azeemphyder123@gmail.com>
2025-11-16 13:04:36 +0530

    login partially completed

--- user/urls.py
+++ user/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from user import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("products/",views.products),
-    path("register/",views.user_register),
-    path("login/",views.user_login)
+    path("login/",views.user_login),
+    path("home/",views.products)
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,27 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render,redirect
-def  products(request):
-    return render(request,"user/user_home.html")
 
-def
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful!")
+
+            if user.role == "seller":
+                return redirect("seller_dashboard")
+
+            return redirect("/user/home")
+
+        messages.error(request, "Invalid username or password")
+        return redirect("/user/login")
+
+    return render(request, "user/login.html")
+
+def products(request):
+    return render(request,"user/user_home.html")
