c815411a09426aa439d6d07f9feb017e9feb2adb
Athul666MS <athulsivanandan294@gmail.com>
2025-11-17 17:23:30 +0530

    dev chnaged only features is left

--- user/urls.py
+++ user/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from user import views
 
 urlpatterns = [
-   
+
     path("products/",views.products),
-    path("register/",views.user_register),
-    path("login/",views.user_login)
+    # path("register/",views.user_register),
+    # path("login/",views.user_login)
 
 ]
--- user/views.py
+++ user/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,redirect
 def  products(request):
     return render(request,"user/user_home.html")
 
-def
+
