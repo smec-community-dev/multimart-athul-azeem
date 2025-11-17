413a2b6ed31acd9021ff5947f5361aa1243f57cb
Athul666MS <athulsivanandan294@gmail.com>
2025-11-17 10:38:11 +0530

    logout completed

--- seller/urls.py
+++ seller/urls.py
@@ -7,8 +7,10 @@ from seller import views
 urlpatterns = [
 
     path("view/",views.view_product),
-    path('registration/',views.seller_registration),
+    path('home/',views.home,name='home'),
+    path('registration/',views.seller_registration,name='registration'),
     path("seller_dashboard/",views.view_product,name="seller_dashboard"),
-    path("login/",views.login_seller),
+    path("login/",views.login_seller,name='login'),
+    path("logout/", views.logout_seller, name='logout'),
     path("seller_dashboard/add/",views.add_product,name='add')
 ]
