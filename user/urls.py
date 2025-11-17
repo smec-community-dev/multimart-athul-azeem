7221023adba033e380e7f99a217fde8fd9e91ed6
azeem7902 <azeemphyder123@gmail.com>
2025-11-17 12:03:15 +0530

    cart srated

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,6 @@ from user import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("products/",views.products),
-    path("register/",views.user_register),
-    path("login/",views.user_login)
+    path("home/",views.products),
+
 ]
