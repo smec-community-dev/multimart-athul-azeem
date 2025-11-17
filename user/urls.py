33bf04f569bb4bd1a4e81bdaae686e3af535c1d8
azeem7902 <azeemphyder123@gmail.com>
2025-11-17 15:12:54 +0530

    cart partially comleted

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,10 @@ from user import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("home/",views.products),
+    path("product/<slug:slug>/", views.product_detail, name="product_detail"),
+    path("add-to-cart/<slug:slug>",views.add_to_cart,name="add_to_cart"),
+    path("cart/",views.cart_page,name="cart_page"),
+
 
 ]
+
--- user/views.py
+++ user/views.py
@@ -1,4 +1,46 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect
+from seller.models import Product
+from user.models import Cart
+
+
 def  products(request):
     return render(request,"user/user_home.html")
 
+def product_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    return render(request, "user/product_detail.html", {"product": product})
+
+@login_required(login_url="login")
+def add_to_cart(request, slug):
+    try:
+        product = Product.objects.get(slug=slug)
+    except Product.DoesNotExist:
+        messages.error(request, "Product not found")
+        return redirect("user/user_home")
+
+    cart_item, created = Cart.objects.get_or_create(
+        user=request.user,
+        product=product
+    )
+
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    messages.success(request, f"{product.name} added to cart")
+    return redirect("cart_page")
+
+
+
+@login_required(login_url="login")
+def cart_page(request):
+    cart_items = Cart.objects.filter(user=request.user)
+
+    total_amount = sum(item.product.price * item.quantity for item in cart_items)
+
+    return render(request, "user/cart.html", {
+        "cart_items": cart_items,
+        "total": total_amount
+    })
